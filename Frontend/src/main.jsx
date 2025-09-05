73a2226cb589dabe72aec4241788b1aeaef01b1e
HANU KOSHTI <95334298+HANU13@users.noreply.github.com>
2025-09-05 17:49:42 +0530

    Create main.jsx

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Ecommerce3DApp from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Ecommerce3DApp />
+  </React.StrictMode>
+);
